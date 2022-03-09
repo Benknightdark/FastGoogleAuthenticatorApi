6c31c7a3220e623c026f6d2409739cf800399457
ben <moon791017@gmail.com>
2022-03-09 16:38:08 +0800

    OK

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.responses import HTMLResponse,FileResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
@@ -12,16 +12,19 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 class OtpModel(BaseModel):
     username: str
     issuer_name: str
     secret_key: Optional[str]
 
+
 @app.get("/", response_class=HTMLResponse)
 def http(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/otp_uri",summary="回傳Google Authenticator驗證網址")
+
+@app.post("/otp_uri", summary="回傳Google Authenticator驗證網址")
 async def get_otp_uri(data: OtpModel):
     """
     回傳Google Authenticator驗證網址
@@ -33,10 +36,14 @@ async def get_otp_uri(data: OtpModel):
         json: 回傳Google Authenticator驗證網址
     """
     new_secret_key = ''
+    print(data)
     if data.secret_key == None:
         new_secret_key = pyotp.random_base32()
     else:
-        new_secret_key = data.secret_key
+        if data.secret_key == "":
+            new_secret_key = pyotp.random_base32()
+        else:
+            new_secret_key = data.secret_key
     totp = pyotp.totp.TOTP(new_secret_key)
     provisioning_uri = totp.provisioning_uri(
         name=data.username, issuer_name=data.issuer_name)
